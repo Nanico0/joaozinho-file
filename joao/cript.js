1e184e64ee860aa7515e3569d6dd4c5646317a8c
Nanico0 <168193542+Nanico0@users.noreply.github.com>
2024-08-05 11:44:53 -0300

    Add files via upload

---
+++ joao/cript.js
@@ -0,0 +1,5 @@
+const caixaPrincipal= document.querySelector(".caixa-principal");
+const caixaPerguntas=document.querySelector(".caixaPerguntas");
+const caixaAlternativas= document.querySelector(".caixa-alternativas");
+const caixaResultado= document.querySelector(".caixa-resultado");
+const textoResultado=document.querySelector(".caixa-resultado");
