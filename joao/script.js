b65b2eb6e45b31e7f81256bc099a8e2d78e32ba2
Nanico0 <168193542+Nanico0@users.noreply.github.com>
2024-08-19 11:46:40 -0300

    Add files via upload

---
+++ joao/script.js
@@ -0,0 +1,89 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativa");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado:
+        alternativas: [
+            {
+                texto:
+                afirmacao:
+            },
+            {
+                texto:
+                afirmacao:
+            }
+        ]
+    },
+    {
+        enunciado:
+        afirmacao: [
+            {
+                texto:
+                afirmacao:
+            },
+            {
+                texto:
+                afirmacao:
+            }
+        ] 
+        }
+        {
+            anunciado:
+            alternativas: [
+                {
+                    texto:
+                    afirmacao:
+                },
+                {
+                    texto:
+                    afirmacao:
+                }
+            ]
+        },
+        {
+            enunciado:
+            alternativas: [
+            {
+                texto:
+                afirmacao:
+            },
+            {
+                texto:
+                afirmacao:
+            }
+        ]
+     },
+     {
+        enunciado:
+        alternativas: [
+            {
+                texto:
+                afirmacao:
+            },
+            {
+                texto:
+                afirmacao:
+            }
+        ]
+     },
+ ];
+
+
+let atual = 0;
+let perguntasAtual;
+let historiaFinal = "";
+
+function mostraperguntas() {
+    if(atual >= perguntas.length) {}
+}
+             
+                
+                
+            
+    
+
+        
